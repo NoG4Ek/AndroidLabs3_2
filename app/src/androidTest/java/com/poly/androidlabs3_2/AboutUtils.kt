91df81402bfe2e252c2bf703c000eb78f993b132
Kirill Timofeev <kirill.timofeev.a@gmail.com>
2021-11-24 14:59:08 +0300

    fix tests activityAbout

--- app/src/androidTest/java/com/poly/androidlabs3_2/AboutUtils.kt
+++ app/src/androidTest/java/com/poly/androidlabs3_2/AboutUtils.kt
@@ -6,6 +6,7 @@ import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.contrib.DrawerActions
 import androidx.test.espresso.contrib.DrawerMatchers.isClosed
+import androidx.test.espresso.contrib.NavigationViewActions
 import androidx.test.espresso.matcher.ViewMatchers.withId
 
 private fun openAboutViaDrawer() {
@@ -15,7 +16,8 @@ private fun openAboutViaDrawer() {
         .perform(DrawerActions.open()); // Open Drawer
 
     // Start the screen of your activity.
-    onView(withId(R.id.bnToAbout)).perform(click())
+    onView(withId(R.id.nav_view))
+        .perform(NavigationViewActions.navigateTo(R.id.activityAbout))
 }
 
 fun openAbout() = openAboutViaDrawer()
